dd6972b02603f23ea5844f2fe00bcea7c1333b73
Tomáš Korenko <radixxko@gmail.com>
2020-09-02 11:14:50 +0200

    Version 1.0.0

---
+++ test/test.js
@@ -0,0 +1,25 @@
+const Client = require('../lib/client');
+
+async function test()
+{
+    let client = new Client();
+
+    //let res = await new Client().get( 'https://www.google.com' );
+    let res = await client.get( 'https://www.google.com' );
+
+    console.log( res.statusCode, res.ok, res.headers );
+
+    //res.json.then( j => console.log( j.length ))
+    //res.json.then( j => console.log( j.length ))
+    //res.json.then( j => console.log( j.length ))
+
+    //console.log(( await res.json ).length )
+    //console.log(( await res.json ).length )
+    //console.log( await res.json )
+
+    res = await client.get( 'https://www.google.com' );
+
+    console.log( res.statusCode, res.ok, res.headers );
+}
+
+test()
